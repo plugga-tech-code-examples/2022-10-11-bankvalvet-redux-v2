3eb7bb15851b21d0c04fff99e414c87303e548a2
David Jensen <tsourdox@icloud.com>
2022-10-11 10:33:58 +0200

    initial app layout with react navigation

---
+++ navigation/RootNavigator.tsx
@@ -0,0 +1,51 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import Balance from "../screens/BalanceScreen";
+import Deposit from "../screens/DepositScreen";
+import Withdrawal from "../screens/WithdrawalScreen";
+
+type RootTabsParamList = {
+  Balance: undefined;
+  Withdrawal: undefined;
+  Deposit: undefined;
+};
+
+const Tabs = createBottomTabNavigator<RootTabsParamList>();
+
+export default function RootNavigator() {
+  return (
+    <NavigationContainer>
+      <Tabs.Navigator>
+        <Tabs.Screen
+          name="Balance"
+          component={Balance}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="Withdrawal"
+          component={Withdrawal}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="bank-transfer-out" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="Deposit"
+          component={Deposit}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="bank-transfer-in" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tabs.Navigator>
+    </NavigationContainer>
+  );
+}
