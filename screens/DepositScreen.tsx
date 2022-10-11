b7cc08a4d792168ac0520913eea377e3a52a8d3f
David Jensen <tsourdox@icloud.com>
2022-10-11 11:22:31 +0200

    update bank state from components

--- screens/DepositScreen.tsx
+++ screens/DepositScreen.tsx
@@ -1,9 +1,17 @@
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { useAppDispatch } from "../store/store";
 
 export default function Deposit() {
+  const dispatch = useAppDispatch();
+
   return (
     <View style={styles.container}>
       <Text>Deposit</Text>
+      <Button
+        title="Sätt in pengar"
+        onPress={() => dispatch({ type: "bank/deposit", payload: 500 })}
+      />
     </View>
   );
 }
--- screens/WithdrawalScreen.tsx
+++ screens/WithdrawalScreen.tsx
@@ -1,9 +1,16 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { useAppDispatch } from "../store/store";
 
 export default function Withdrawal() {
+  const dispatch = useAppDispatch();
+
   return (
     <View style={styles.container}>
       <Text>Withdrawal</Text>
+      <Button
+        title="Ta ut pengar"
+        onPress={() => dispatch({ type: "bank/withdrawal", payload: 50 })}
+      />
     </View>
   );
 }
