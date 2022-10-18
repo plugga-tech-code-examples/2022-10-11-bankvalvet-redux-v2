b4de9d5e7d30b4e68c3380eea9ede350917be014
David Jensen <tsourdox@icloud.com>
2022-10-18 10:27:34 +0200

    remove old code

--- store/bank/bankSelectors.ts
+++
@@ -1,5 +0,0 @@
-import { AppState } from "../store";
-
-export const selectBank = (state: AppState) => state.bank;
-export const selectBalance = (state: AppState) => selectBank(state).balance;
-export const selectTranscations = (state: AppState) => selectBank(state).transactions;
